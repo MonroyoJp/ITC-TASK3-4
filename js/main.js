7cc4cf920ae987ec68467da9e999d78ccd65a9fd
JP Monroyo <johnpaulmonroyo14@gmail.com>
2025-10-06 01:39:49 +0800

    edit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+const top = 0;
+const topNav = document.querySelector(".topNav-flex");
+let topNavPos = topNav.getBoundingClientRect().top;
+let currentYPos;
+let topNavHeight = topNav.offsetHeight;
+const fixedNavClass = "nav-fixed-bg";
 
+const mainWrapper = document.querySelector(".mainWrapper");
+
+window.addEventListener("scroll", ()=>{
+    currentYPos = top + window.scrollY;
+    console.log(currentYPos + " Hello " + topNavPos);
+    currentYPos >= topNavPos +  topNavHeight ? topNav.classList.add(fixedNavClass):topNav.classList.remove(fixedNavClass)
+});
